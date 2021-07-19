28a9cdc442bc70af23603fd0972841ad13dbe24e
Бабайкин Вася <C:\Users\Vasiliy Babaykin\AppData\Roaming\The Bat!>
2021-07-19 13:04:35 +0400

    commit

--- insurance/form.py
+++ insurance/form.py
@@ -7,6 +7,7 @@ class Request_for_a_call_Form(forms.Form):
         model = Request_for_a_call
         fields = ('name','phone_number','services', 'comment')
 
+
     @property
     def errors(self):
         """Return an ErrorDict for the data provided for the form."""
@@ -37,3 +38,18 @@ class Request_for_a_call_Form(forms.Form):
                                          comment = self.data.get('comment'))
         one_request.save()
         return 0
+
+class Search_Services(forms.Form):
+    def добавить_поле_все_для_выбора( список_выборов):
+        итоговой_результат = [("Все", "Все")]
+        for один_выбор in список_выборов:
+            итоговой_результат.append(один_выбор)
+        return итоговой_результат
+
+    type = forms.ChoiceField(label = "тип сервиса",
+                             choices = добавить_поле_все_для_выбора(list(map(lambda one_services: (one_services.name, one_services.name), Type_services.objects.all()))) )
+    rating = forms.ChoiceField(label = "экспертный рейтинг",
+                               choices=добавить_поле_все_для_выбора(Insurance_companies.RATING_CHOICES))
+    class Meta:
+        model = Services
+        fields = ('insurance_companies','description','insurance_cost', 'amount_of_payments',"terms_of_insurance",)
--- insurance/views.py
+++ insurance/views.py
@@ -8,9 +8,20 @@ class main(ListView):
     model = Services
     template_name = 'insurance/index.html'
     context_object_name = 'Services'
+
     def get_queryset(self):
-        a = Services.objects.all()
-        return a
+        # original qs
+        Search_Services(data = self.request.GET)
+        qs = super().get_queryset()
+        return qs
+        # filter by a variable captured from url, for example
+        return qs.filter(name__startswith=self.kwargs['name'])
+
+
+    def get_context_data(self, **kwargs):
+        context = super(main, self).get_context_data(**kwargs)
+        context['form'] = Search_Services(data = self.request.GET)
+        return context
 @csrf_exempt
 def add_request_for_a_call(request):
     form = Request_for_a_call_Form(request.POST)

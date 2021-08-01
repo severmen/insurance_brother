cf52f7774c7de3d5c9e7a3e315a3975c0629f12d
Бабайкин Вася <C:\Users\Vasiliy Babaykin\AppData\Roaming\The Bat!>
2021-08-01 12:32:52 +0400

    добалены дополнительные тесты

--- insurance/tests.py
+++ insurance/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 import pika
 import os
-# Create your tests here.
+import datetime
+from .models import Insurance_companies,Type_services,Services,Request_for_a_call
+from django.contrib.auth.models import User
+
 
 class baseSystemTest(TestCase):
 
@@ -38,6 +41,70 @@ class baseSystemTest(TestCase):
         send()
         get()
         self.assertEqual(RabbitMQ_request_result, "test message")
+
+    def test_models(self):
+        '''
+        тест заполлнения моделей
+        '''
+        user = None
+        company = None
+        type = None
+        service = None
+        request = None
+        def create_user():
+            nonlocal user
+            user = User(username = "test",
+                    password = "test")
+            user.save()
+
+        def create_companies():
+            nonlocal company
+            company = Insurance_companies(autor = user,
+                            name = "тестовая компания",
+                            expert_rating="A",
+                            customer_base=1,
+                            general_refusal=1.1,
+                            date_of_creation=datetime.datetime.now())
+            company.save()
+
+        def creaete_type():
+            nonlocal type
+            type = Type_services(name = "тестовый тип сервиса")
+            type.save()
+
+        def crete_service():
+            nonlocal service
+            service = Services(
+                insurance_companies=company,
+                type_services = type,
+                name = "Название тестового сервиса",
+                description="Описание тестовго сервиса",
+                insurance_cost= 100,
+                amount_of_payments=110,
+                terms_of_insurance = "много" )
+            service.save()
+
+        def crete_request():
+            nonlocal request
+            request = Request_for_a_call(name = "Тестовое имя",
+                                         phone_number="12345",
+                                         services = service,
+                                         comment = "тестовый комментарий")
+            request.save()
+
+        create_user()
+        create_companies()
+        creaete_type()
+        crete_service()
+        crete_request()
+
+        self.assertEqual(request.name, "Тестовое имя")
+        self.assertEqual(service.name, "Название тестового сервиса")
+        self.assertEqual(type.name, "тестовый тип сервиса")
+        self.assertEqual(company.name, "тестовая компания")
+        self.assertEqual(user.username, "test")
+
+
 
 
 
